679982e83afff24c29f071d4f3a75a78eca92d6a
Steffen Thielemans <steffen.thielemans@gmail.com>
2019-10-28 12:04:38 +0100

    Included old raw multicast control to pwmcoap.

--- project/pwmcoap/pwmcoap.c
+++ project/pwmcoap/pwmcoap.c
@@ -17,9 +17,8 @@
 #include "net/ipv6/uip-debug.h"
 /*---------------------------------------------------------------------------*/
 
-/*
-#define UDP_CLIENT_PORT	0  	//MUST BE 0 TO ALLOW UDP PACKETS WITH RANDOM SRC PORT
-#define UDP_SERVER_PORT	6000	//LISTENING ON THIS UDP DEST PORT
+
+#define UDP_SERVER_PORT	6000	//LISTENING ON THIS UDP DEST PORT FOR MULTICAST
 
 static struct uip_udp_conn *server_conn;
 
@@ -27,40 +26,40 @@ static struct uip_udp_conn *server_conn;
 #error "This example can not work with the current contiki configuration"
 #error "Check the values of: NETSTACK_CONF_WITH_IPV6, UIP_CONF_ROUTER, UIP_CONF_IPV6_RPL"
 #endif
-*/
+
 
 extern coap_resource_t pwm_light;
-
+void pwm_init_handler();
+void pwm_update(uint16_t newdutycycle);
 
 PROCESS(pwm_multicast, "pwm multicast");
 AUTOSTART_PROCESSES(&pwm_multicast);
 /*---------------------------------------------------------------------------*/
 
-/*
+
 static void
 tcpip_handler(void)
 {
+	PRINTF("RECEIVED MULTICAST FROM: ");
+	PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+	PRINTF("\n");
+
   if(uip_newdata()) {
 
 	uint16_t* newdutycycle = (uint16_t *)uip_appdata; //We use promille instead of %
 
-	//PRINTF("Recv duty cycle %u promille from ", *newdutycycle);
-	//PRINT6ADDR(&UIP_UDPIP_BUF->srcipaddr);
-	//PRINTF(" on port %u\n", UIP_HTONS(UIP_UDPIP_BUF->destport));
-
 	if(*newdutycycle <= 1000) {
-		dutycycle = *newdutycycle;
-		pwm_startdutycycle(&ledDriverPWM, dutycycle);
-		PRINTF("Set PWM on timer %u/%u to duty cycle %u promille\n",ledDriverPWM.timer, ledDriverPWM.ab, dutycycle);
+		pwm_update(*newdutycycle);
+		PRINTF("Set PWM to duty cycle %u promille\n", *newdutycycle);
 	}
 
   } 
-}*/
+}
 
 
 /*---------------------------------------------------------------------------*/
 
-/*
+
 static uip_ds6_maddr_t *
 join_mcast_group(void)
 {
@@ -75,7 +74,7 @@ join_mcast_group(void)
     PRINTF("\n");
   }
   return rv;
-} */
+}
 /*---------------------------------------------------------------------------*/
 //static void
 //unjoin_mcast_group(void){
@@ -88,19 +87,18 @@ join_mcast_group(void)
 //}
 /*---------------------------------------------------------------------------*/
 
-void pwm_init_handler();
 
 PROCESS_THREAD(pwm_multicast, ev, data)
 {
   PROCESS_BEGIN();
 
 
-/*  if(join_mcast_group() == NULL) {
+  if(join_mcast_group() == NULL) {
     PRINTF("Failed to join multicast group\n");
     PROCESS_EXIT();
   }
 
-  server_conn = udp_new(NULL, UIP_HTONS(UDP_CLIENT_PORT), NULL);
+  server_conn = udp_new(NULL, 0, NULL);
   if(server_conn == NULL) {
     PRINTF("No UDP connection available, exiting the process!\n");
     PROCESS_EXIT();
@@ -111,20 +109,20 @@ PROCESS_THREAD(pwm_multicast, ev, data)
   PRINT6ADDR(&server_conn->ripaddr);
   PRINTF(" local/remote port %u/%u\n", UIP_HTONS(server_conn->lport),
          UIP_HTONS(server_conn->rport));
-*/
+
 
 
   //CoAP stuff here
   coap_activate_resource(&pwm_light, "lamp/dimming");
   pwm_init_handler();
 
-/*
+
   while(1) {
     PROCESS_YIELD();
     if(ev == tcpip_event) {
       tcpip_handler();
     }
-  }*/
+  }
 
   PROCESS_END();
 

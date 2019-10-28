a2040b8e23d40b05bc19bcb76273e37724706eaf
Steffen Thielemans <steffen.thielemans@gmail.com>
2019-10-28 19:12:22 +0100

    Migrated to only CoAP group communication instead of also standalone multicast processing.

--- project/pwmcoap/pwmcoap.c
+++ project/pwmcoap/pwmcoap.c
@@ -1,15 +1,5 @@
 #include "contiki.h"
-#include "cpu.h"
-#include "dev/leds.h"
-#include "dev/watchdog.h"
-#include "dev/sys-ctrl.h"
-#include "pwm.h"
-#include "lpm.h"
-#include "dev/ioc.h"
 #include "contiki-net.h"
-#include "net/ipv6/uip.h"
-#include "net/packetbuf.h"
-
 #include "coap-engine.h"
 
 /*---------------------------------------------------------------------------*/
@@ -17,49 +7,17 @@
 #include "net/ipv6/uip-debug.h"
 /*---------------------------------------------------------------------------*/
 
-
-#define UDP_SERVER_PORT	6000	//LISTENING ON THIS UDP DEST PORT FOR MULTICAST
-
-static struct uip_udp_conn *server_conn;
-
 #if !NETSTACK_CONF_WITH_IPV6 || !UIP_CONF_ROUTER || !UIP_IPV6_MULTICAST || !UIP_CONF_IPV6_RPL
 #error "This example can not work with the current contiki configuration"
 #error "Check the values of: NETSTACK_CONF_WITH_IPV6, UIP_CONF_ROUTER, UIP_CONF_IPV6_RPL"
 #endif
 
-
 extern coap_resource_t pwm_light;
 void pwm_init_handler();
-void pwm_update(uint16_t newdutycycle);
 
 PROCESS(pwm_multicast, "pwm multicast");
 AUTOSTART_PROCESSES(&pwm_multicast);
 /*---------------------------------------------------------------------------*/
-
-
-static void
-tcpip_handler(void)
-{
-	PRINTF("RECEIVED MULTICAST FROM: ");
-	PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
-	PRINTF("\n");
-
-  if(uip_newdata()) {
-
-	uint16_t* newdutycycle = (uint16_t *)uip_appdata; //We use promille instead of %
-
-	if(*newdutycycle <= 1000) {
-		pwm_update(*newdutycycle);
-		PRINTF("Set PWM to duty cycle %u promille\n", *newdutycycle);
-	}
-
-  } 
-}
-
-
-/*---------------------------------------------------------------------------*/
-
-
 static uip_ds6_maddr_t *
 join_mcast_group(void)
 {
@@ -92,37 +50,17 @@ PROCESS_THREAD(pwm_multicast, ev, data)
 {
   PROCESS_BEGIN();
 
+  //Initialize lamp PWM control
+  pwm_init_handler();
 
+  //Join multicast group for CoAP group communication
   if(join_mcast_group() == NULL) {
     PRINTF("Failed to join multicast group\n");
     PROCESS_EXIT();
   }
 
-  server_conn = udp_new(NULL, 0, NULL);
-  if(server_conn == NULL) {
-    PRINTF("No UDP connection available, exiting the process!\n");
-    PROCESS_EXIT();
-  }
-  udp_bind(server_conn, UIP_HTONS(UDP_SERVER_PORT));
-
-  PRINTF("Created a server connection with remote address ");
-  PRINT6ADDR(&server_conn->ripaddr);
-  PRINTF(" local/remote port %u/%u\n", UIP_HTONS(server_conn->lport),
-         UIP_HTONS(server_conn->rport));
-
-
-
-  //CoAP stuff here
+  //Activate CoAP resource
   coap_activate_resource(&pwm_light, "lamp/dimming");
-  pwm_init_handler();
-
-
-  while(1) {
-    PROCESS_YIELD();
-    if(ev == tcpip_event) {
-      tcpip_handler();
-    }
-  }
 
   PROCESS_END();
 

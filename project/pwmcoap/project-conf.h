a880904ea404a3b9e5f4c0fa9727f62068ce77ef
Steffen Thielemans <steffen.thielemans@gmail.com>
2019-12-11 13:46:17 +0100

    Updated project configurations to more closely represent the lamp multicast setup of the telecom lab.

--- project/pwmcoap/project-conf.h
+++ project/pwmcoap/project-conf.h
@@ -13,6 +13,16 @@
 #define LPM_CONF_ENABLE 0  //Absolute must for enabling 32KB RAM!
 
 
+#define CC2538_RF_CONF_TX_POWER 		0xFF  //Default 0xd5: +3dBm,  0xff: +7dBm
+
+//#define CC2538_RF_CONF_CHANNEL              26
+
+#undef LLSEC802154_CONF_ENABLED
+#define LLSEC802154_CONF_ENABLED          1
+
+#define CSMA_CONF_LLSEC_DEFAULT_KEY0 { 0xAA , 0xBB , 0xCC , 0xDD , 0xEE , 0xFF , 0x00 , 0x11 , 0x22 , 0x33 , 0x44 , 0x55 , 0x66 , 0x77 , 0x88 , 0x99 }
+
+
 
 #endif /* PROJECT_CONF_H_ */
 
--- project/rpl-border-router-BMRF/project-conf.h
+++ project/rpl-border-router-BMRF/project-conf.h
@@ -31,12 +31,15 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#define ROUTING_CONF_RPL_CLASSIC 1
+//#define ROUTING_CONF_RPL_CLASSIC 1
 
 
 //Use Zolertia Re-Mote second USB port for direct USB communication instead of serial
-#define SLIP_ARCH_CONF_USB 1
-#define DBG_CONF_USB 1
+//#define SLIP_ARCH_CONF_USB 1
+//#define DBG_CONF_USB 1
+
+//#define LOG_CONF_LEVEL_RPL LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_IPV6 LOG_LEVEL_DBG
 
 
 #include "net/ipv6/multicast/slip-multicast.h"  //Enables custom slip input handler
@@ -44,34 +47,34 @@
 
 //#define BMRF_NETSTACK_SNIFFER 1
 
-
 /* Change this to switch engines. Engine codes in uip-mcast6-engines.h */
 #define UIP_MCAST6_CONF_ENGINE	UIP_MCAST6_ENGINE_BMRF
 #define BMRF_CONF_MODE	BMRF_UNICAST_MODE
+
 //#define BMRF_CONF_MODE	BMRF_BROADCAST_MODE
 //#define BMRF_CONF_MODE	BMRF_MIXED_MODE
 //#define BMRF_CONF_BROADCAST_THRESHOLD	1
 
-#define UIP_MCAST6_ROUTE_CONF_ROUTES 15
-//#define CSMA_CONF_MAX_NEIGHBOR_QUEUES UIP_MCAST6_ROUTE_CONF_ROUTES
+#define UIP_MCAST6_ROUTE_CONF_ROUTES 20
+#define CSMA_CONF_MAX_NEIGHBOR_QUEUES UIP_MCAST6_ROUTE_CONF_ROUTES
 
 /* Code/RAM footprint savings so that things will fit on our device */
 #ifndef NETSTACK_MAX_ROUTE_ENTRIES
-#define NETSTACK_MAX_ROUTE_ENTRIES   15
+#define NETSTACK_MAX_ROUTE_ENTRIES   20
 #endif
 
 #ifndef NBR_TABLE_CONF_MAX_NEIGHBORS
-#define NBR_TABLE_CONF_MAX_NEIGHBORS 15
+#define NBR_TABLE_CONF_MAX_NEIGHBORS 20
 #endif
 
 //#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
-#define CC2538_RF_CONF_TX_POWER 		0xFF  //Default 0xd5: +3dBm,  0xff: +7dBm
+//#define CC2538_RF_CONF_TX_POWER 		0xFF  //Default 0xd5: +3dBm,  0xff: +7dBm
 
 
-#define SICSLOWPAN_CONF_FRAG 0
-#define QUEUEBUF_CONF_NUM 15
+//#define SICSLOWPAN_CONF_FRAG 0
+#define QUEUEBUF_CONF_NUM 20
 
-#define UIP_CONF_BUFFER_SIZE 128
+//#define UIP_CONF_BUFFER_SIZE 128
 
 //#define TSCH_CONF_ASSOCIATION_POLL_FREQUENCY 20
 //#define TSCH_CONF_CHANNEL_SCAN_DURATION CLOCK_SECOND*5
@@ -80,7 +83,7 @@
 
 #define LPM_CONF_ENABLE 0  //Absolute must for enabling 32KB RAM!
 
-#define SLIP_CONF_WITH_STATS 0
+//#define SLIP_CONF_WITH_STATS 0
 
 
 #ifndef WEBSERVER_CONF_CFS_CONNS
@@ -94,5 +97,16 @@
 #if BORDER_ROUTER_CONF_WEBSERVER
 #define UIP_CONF_TCP 1
 #endif
+
+
+#define CC2538_RF_CONF_TX_POWER 		0xFF  //Default 0xd5: +3dBm,  0xff: +7dBm
+
+//#define CC2538_RF_CONF_CHANNEL              26
+
+#undef LLSEC802154_CONF_ENABLED
+#define LLSEC802154_CONF_ENABLED          1
+
+#define CSMA_CONF_LLSEC_DEFAULT_KEY0 { 0xAA , 0xBB , 0xCC , 0xDD , 0xEE , 0xFF , 0x00 , 0x11 , 0x22 , 0x33 , 0x44 , 0x55 , 0x66 , 0x77 , 0x88 , 0x99 }
+
 
 #endif /* PROJECT_CONF_H_ */
